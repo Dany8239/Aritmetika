45e0d007870024170f0acaa617eab1e31b152d63
Dany8239 <dany.koch.9@gmail.com>
2025-02-26 09:09:08 +0100

    Pridano 5 obtiznosti

--- aritmetika.py
+++ aritmetika.py
@@ -1,37 +1,48 @@
 import time as t
 import random
-nums = []
-multnums = []
-mistakes = 0
 print("Vitej v aritmeticke hre!")
 while True:
     try:
         nums = []
         multnums = []
-        mistakes = 0
-        n = int(input("Jaka je maximalni hodnota, se kterou chces pocitat?: "))
-        limit = input("Chces omezit nasobeni na malou nasobilku?: [a]no/[n]e: ")
+        multn = 0
+        n = 0
+        diff = input("Vyber obtiznost: [b]aby, [e]asy, [m]edium, [h]ard, e[x]treme, [c]ustom: ")
+        if diff.lower() == "c":
+            n = int(input("Jaka je maximalni hodnota, se kterou chces pocitat?: "))
+            limit = input("Chces omezit nasobeni na malou nasobilku?: [a]no/[n]e: ")
+        if diff.lower() == "b":
+            n = 100
+            limit = "a"
+        if diff.lower() == "e":
+            n = 1000
+            multn = 15
+        if diff.lower() == "m":
+            n = 10000
+            multn = 50
+        if diff.lower() == "h":
+            n = 100000
+            multn = 100
+        if diff.lower == "e":
+            n = 1000000
+            multn = 200
+
         rounds = int(input("Kolik chces hrat kol?: "))
-        if n <= 0 or rounds <= 0:
-            print("Zadej kladne cislo!")
-            continue
+        if limit.lower() == "a":
+            for i in range(1, 11):
+                multnums.append(i)
+        else:
+            for i in range(multn):
+                multnums.append(i+1)
+        totaltime = 0
+        for i in range(n):
+            nums.append(i+1)
         print("Zacinas za 3...")
         t.sleep(1)
         print("Zacinas za 2...")
         t.sleep(1)
         print("Zacinas za 1...")
         t.sleep(1)
-        if limit.lower() == "a":
-            for i in range(11):
-                multnums.append(i)
-            multn = 9
-        else:
-            for i in range(n+1):
-                multn = n
-        print(multnums)
-        totaltime = 0
-        for i in range(n+1):
-            nums.append(i)
         for i in range(rounds):
             op = random.randint(1, 4)
             idx1 = random.randint(0, n-1)
@@ -49,7 +60,6 @@ while True:
                         totaltime += end - start
                     else:
                         print("Spatne! Zkus to znovu.")
-                        mistakes += 1
             elif op == 2:
                 answered = False
                 answer = nums[idx1] - nums[idx2]
@@ -63,11 +73,10 @@ while True:
                         totaltime += end - start
                     else:
                         print("Spatne! Zkus to znovu.")
-                        mistakes += 1
             elif op == 3:
                 answered = False
-                idx1 = random.randint(0, multn-1)
-                idx2 = random.randint(0, multn-1)
+                idx1 = random.randint(0, 9)
+                idx2 = random.randint(0, 9)
                 answer = multnums[idx1] * multnums[idx2]
                 while not answered:
                     start = t.time()
@@ -79,7 +88,6 @@ while True:
                         totaltime += end - start
                     else:
                         print("Spatne! Zkus to znovu.")
-                        mistakes += 1
             elif op == 4:
                 answered = False
                 while nums[idx1] % nums[idx2] != 0:
@@ -96,9 +104,7 @@ while True:
                         totaltime += end - start
                     else:
                         print("Spatne! Zkus to znovu.")
-                        mistakes += 1
         print(f"Tvuj celkovy cas byl: {round(totaltime, 1)} sekund, prumerny cas na odpoved byl: {round(totaltime/rounds, 1)} sekund.")
-        print(f"Udelal jsi {mistakes} chyb. Tvoje presnost byla {round((rounds-mistakes)/rounds*100, 1)}%.")
         repeat = input("Chces hrat znovu? ([a]no/[n]e): ")
         if repeat.lower() == "a":
             continue
